2eaa01bdc68e196ee8b130d3a9c891c9ca4817ce
irakey <102231888+irakey@users.noreply.github.com>
2024-11-20 13:01:56 +0100

    Delete locators directory

--- locators/logo_locators.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class LogoLocators:
-    LOGO_YANDEX = By.XPATH, ".//*[@class='Header_LogoYandex__3TSOI']"
-    LOGO_SCOOTER = By.XPATH, ".//*[@class='Header_LogoScooter__3lsAR']"
-    HEADER_DZEN = By.ID, "dzen-header"
-
